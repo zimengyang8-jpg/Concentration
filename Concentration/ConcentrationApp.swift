7eccb57c74132d88e6a0b7e7d90c7f157645b3f3
Zimeng Yang <zimengyang8@gmail.com>
2026-03-15 22:50:56 -0400

    Initial Commit

---
+++ Concentration/ConcentrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcentrationApp.swift
+//  Concentration
+//
+//  Created by Zimeng Yang on 3/15/26.
+//
+
+import SwiftUI
+
+@main
+struct ConcentrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Concentration/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Concentration
+//
+//  Created by Zimeng Yang on 3/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
